4c17821ee7d3aa75f0d7de0c9f74e4eb6ceb8b2b
forfin <porraphit@gmail.com>
2020-04-22 21:18:34 +0700

    Add message to model.

--- Forfin/LINENotify/Api/Data/LineNotifyLogsInterface.php
+++ Forfin/LINENotify/Api/Data/LineNotifyLogsInterface.php
@@ -1,17 +1,17 @@
 <?php declare(strict_types=1);
 /**
- * Copyright (c) 2019  
- * 
+ * Copyright (c) 2019
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -28,6 +28,7 @@ interface LineNotifyLogsInterface extends \Magento\Framework\Api\ExtensibleDataI
 {
 
     const CREATED_AT = 'created_at';
+    const MESSAGE = 'message';
     const LINENOTIFYLOGS_ID = 'linenotifylogs_id';
     const STATUS = 'status';
     const ORDER_ID = 'order_id';
@@ -44,6 +45,19 @@ interface LineNotifyLogsInterface extends \Magento\Framework\Api\ExtensibleDataI
      * @return \Forfin\LINENotify\Api\Data\LineNotifyLogsInterface
      */
     public function setLinenotifylogsId($linenotifylogsId);
+
+    /**
+     * Get message
+     * @return string|null
+     */
+    public function getMessage();
+
+    /**
+     * Set message
+     * @param string $createdAt
+     * @return \Forfin\LINENotify\Api\Data\LineNotifyLogsInterface
+     */
+    public function setMessage($createdAt);
 
     /**
      * Get created_at
--- Forfin/LINENotify/Model/Data/LineNotifyLogs.php
+++ Forfin/LINENotify/Model/Data/LineNotifyLogs.php
@@ -1,17 +1,17 @@
 <?php declare(strict_types=1);
 /**
- * Copyright (c) 2019  
- * 
+ * Copyright (c) 2019
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -46,6 +46,25 @@ class LineNotifyLogs extends \Magento\Framework\Api\AbstractExtensibleObject imp
     public function setLinenotifylogsId($linenotifylogsId)
     {
         return $this->setData(self::LINENOTIFYLOGS_ID, $linenotifylogsId);
+    }
+
+    /**
+     * Get message
+     * @return string|null
+     */
+    public function getMessage()
+    {
+        return $this->_get(self::MESSAGE);
+    }
+
+    /**
+     * Set message
+     * @param string $message
+     * @return \Forfin\LINENotify\Api\Data\LineNotifyLogsInterface
+     */
+    public function setMessage($message)
+    {
+        return $this->setData(self::MESSAGE, $message);
     }
 
     /**
